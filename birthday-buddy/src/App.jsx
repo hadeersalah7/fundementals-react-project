bc7b54144610b353eec186c9fcc20eb405442c9d
hadeersalah7 <hadeermhmd70@gmail.com>
2024-09-25 10:39:55 +0300

    adding list using state and map

--- birthday-buddy/src/App.jsx
+++ birthday-buddy/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import BirthdayComp from './BirthdayComp'
 function App() {
 
 
   return (
     <>
       <div>
-        
+        <BirthdayComp />
       </div>
     </>
   )
