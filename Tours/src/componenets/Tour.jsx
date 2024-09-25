618557912259544eb8bcf9915d5ac2148a9a29d5
hadeersalah7 <hadeermhmd70@gmail.com>
2024-09-25 16:05:09 +0300

    adding read more

--- Tours/src/componenets/Tour.jsx
+++ Tours/src/componenets/Tour.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Tour = ({id, image, info, price, name, removeTour}) => {
+    const [readMore, setReadMore] = useState(false)
     return (
         <article className="single-tour">
             <img src={image} alt={name} className='img' />
             <span className='tour-price'>${price}</span>
             <div className="tour-info">
                 <h5>{name}</h5>
-                <p>{info}</p>
+                <p>{readMore ? info : `${info.substring(200)}... `}</p>
+                <button type='button' onClick={() => setReadMore(!readMore)} className='info-btn'>
+                    {readMore ? 'Show Less' : "Read More"}
+                </button>
             <button className='btn btn-block delete-btn' type='button' onClick={() => removeTour(id)}>not interested</button>
             </div>
         </article>
