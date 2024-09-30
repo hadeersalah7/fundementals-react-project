5381b156ddfeabef76ae119ddfe5e3b1640dd568
hadeersalah7 <hadeermhmd70@gmail.com>
2024-09-30 09:39:11 +0300

    finshing the functionality of prev & next

--- Carousal/src/components/Carousel.jsx
+++ Carousal/src/components/Carousel.jsx
@@ -3,22 +3,38 @@ import { FaQuoteRight } from "react-icons/fa"
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
 import { list, longList, shortList } from "../data"
 const Carousel = () => {
-    const [people, setPeople] = useState(list)
-    const prevBtn = () => { }
-    const nextBtn = () => {}
+    const [people, setPeople] = useState(longList)
+    const [currentPerson, setCurrentPerson] = useState(0)
+    const prevBtn = () => {
+        setCurrentPerson((oldPerson) => {
+            const result = (oldPerson - 1 + people.length) % people.length
+            return result
+        })
+    }
+    const nextBtn = () => {
+        setCurrentPerson((oldPerson) => {
+            const result = (oldPerson + 1) % people.length
+            return result
+        })
+
+    }
     return (
         <section className='slider-container'>
             {people.map((p, pIndex) => {
                 const { id, image, name, quote, title } = p
                 return (
                     <article className='slide' key={id}
-                    style={{ transform: `translateX(${100 * pIndex}%)` }}
+                        style={{
+                            transform: `translateX(${100 * (pIndex - currentPerson)}%)`,
+                            opacity: pIndex === currentPerson ? 1 : 0,
+                            overflow: pIndex === currentPerson ? 1 : 0
+                        }}
                     >
                         <img src={image} alt={title} className='person-img' />
                         <h5 className='name'>{name}</h5>
                         <p className='title'>{title}</p>
-                        <p className='text'>{quote }</p>
-                    <FaQuoteRight className='icon'/>
+                        <p className='text'>{quote}</p>
+                        <FaQuoteRight className='icon' />
                     </article>
                 )
             })}
