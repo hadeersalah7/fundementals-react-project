8648b8a8b041edc5425b303f514f82422ab7c9ff
hadeersalah <hadeersalah127@gmail.com>
2024-10-04 14:57:26 +0300

    practising first request

--- Axios/src/examples/1-first-request.js
+++ Axios/src/examples/1-first-request.js
@@ -1,10 +1,22 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 // limit, if 429 wait for 15 min and try again
 const url = 'https://www.course-api.com/react-store-products';
 
+const fetchData = async () => {
+  try {
+    const response = await axios(url);
+    const data = response.data;
+    console.log(data)
+  } catch (error) {
+    console.log(error.response)
+  }
+  
+}
+
 const FirstRequest = () => {
   useEffect(() => {
-    console.log('first axios request');
+fetchData()
   }, []);
 
   return <h2 className='text-center'>first request</h2>;
