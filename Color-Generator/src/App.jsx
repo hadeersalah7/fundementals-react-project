f4203a55c6610fd60625fa706eb72b76b22ced57
hadeersalah7 <hadeermhmd70@gmail.com>
2024-10-13 15:49:53 +0300

    adding the onSubimt function from the App

--- Color-Generator/src/App.jsx
+++ Color-Generator/src/App.jsx
@@ -5,8 +5,19 @@ import Values from "values.js"
 import {ToastContainer, toast} from "react-toastify"
 const App = () => {
   const [colors, setColors] = useState(new Values("#f15025").all(10))
+
+  const addColor = (color) => {
+    try {
+      const newColor = new Values(color).all(10)
+    setColors(newColor)
+    } catch (error) {
+      // console.log(error)
+      toast.error(error.message)
+    }
+    
+  }
   return <main>
-    <Form />
+    <Form addColor={addColor}/>
     <ColorList colors={colors} />
     <ToastContainer position="top-center"/>
   </main>;
