81aa17a70b17373273c225e89c5e81b5e505021c
hadeersalah7 <hadeermhmd70@gmail.com>
2024-11-05 14:40:53 +0200

    finshing the modal component

--- Sidebar/src/components/Modal.jsx
+++ Sidebar/src/components/Modal.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./AppContext";
 const Modal = () => {
+  const { showModal, handleCloseModal } = useGlobalContext();
   return (
-    <div>Modal</div>
-  )
-}
+    <div className={showModal ? "modal-overlay showModal" : "modal-overlay"}>
+      <div className="modal-container">
+        <h3>Modal Content</h3>
+        <button className="close-modal-btn" onClick={handleCloseModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
