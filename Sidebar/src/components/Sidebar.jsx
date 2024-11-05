76f97b19c9d7641a321f811290e44d968ac7ed1c
hadeersalah7 <hadeermhmd70@gmail.com>
2024-11-05 15:21:55 +0200

    adding the sidebar component

--- Sidebar/src/components/Sidebar.jsx
+++ Sidebar/src/components/Sidebar.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import logo from "../logo.svg";
+import { links, social } from "../data";
+import { useGlobalContext } from "./AppContext";
 const Sidebar = () => {
+  const { showSidebar, handleCloseSidebar } = useGlobalContext();
   return (
-    <div>Sidebar</div>
-  )
-}
+    <aside className="sidebar">
+      <div className="sidebar-h">
+        <img src={logo} alt="logo" className="logo" />
+        <button type="button" className="close=btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, icon, text } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
 
-export default Sidebar
+      <ul className="social-links">
+        {social.map((s) => {
+          const { id, url, icon } = s;
+          return <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
