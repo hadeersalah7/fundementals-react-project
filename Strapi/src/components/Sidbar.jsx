abdc77fe599bc91823a562d8832507a015572952
hadeersalah7 <hadeermhmd70@gmail.com>
2024-11-06 11:14:44 +0200

    adding the sidebar component

--- Strapi/src/components/Sidbar.jsx
+++ Strapi/src/components/Sidbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "../data";
+import { useGlobalContext } from "./Context";
 const Sidbar = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
   return (
-    <h2>Sidbar</h2>
-  )
-}
+    <aside className={isModalOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-container">
+        <button onClick={closeModal} className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item) => {
+            const { page, pageId, links } = item;
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { url, id, icon, label } = link;
+                    return (
+                      <a key={id} href={url}>
+                        {icon} {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
 
-export default Sidbar
+export default Sidbar;
