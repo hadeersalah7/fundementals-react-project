596405954edef526d255af597cc813f9e144d340
hadeersalah7 <hadeermhmd70@gmail.com>
2024-11-18 14:47:07 +0200

    using matchMedia method to figure userPreference

--- Unsplash Images With React/src/context.jsx
+++ Unsplash Images With React/src/context.jsx
@@ -2,16 +2,29 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 
+const getIntialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)')?.matches
+    console.log(prefersDarkMode)
+    return prefersDarkMode
+}
+  
 export const AppProvider = ({ children }) => {
+  
   const [isDarkTheme, setIsDarkTheme] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('cat')
+  (getIntialDarkMode())
+  const [searchTerm, setSearchTerm] = useState("cat");
+
   const toggleTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-      document.body.classList.toggle("dark-theme", newDarkTheme);
   };
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme])
   return (
-    <AppContext.Provider value={{ isDarkTheme, toggleTheme, searchTerm, setSearchTerm }}>
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleTheme, searchTerm, setSearchTerm }}
+    >
       {children}
     </AppContext.Provider>
   );
