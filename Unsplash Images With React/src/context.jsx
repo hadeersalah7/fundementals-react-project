7da790832ecf5153ba1b144079846a97165e8821
hadeersalah7 <hadeermhmd70@gmail.com>
2024-11-18 14:56:59 +0200

    using the matchMedia to set user's mode preference

--- Unsplash Images With React/src/context.jsx
+++ Unsplash Images With React/src/context.jsx
@@ -2,14 +2,23 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 
+const getIntialDarkMode = () => {
+  const preferedDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+  console.log(preferedDarkMode)
+  return preferedDarkMode
+}
+
 export const AppProvider = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
+  getIntialDarkMode()
   const [searchTerm, setSearchTerm] = useState('cat')
   const toggleTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
-      document.body.classList.toggle("dark-theme", newDarkTheme);
   };
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme])
   return (
     <AppContext.Provider value={{ isDarkTheme, toggleTheme, searchTerm, setSearchTerm }}>
       {children}
