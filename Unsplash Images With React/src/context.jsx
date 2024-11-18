8abd3bddacbc89bf46453920e7eee2858c71f114
hadeersalah7 <hadeermhmd70@gmail.com>
2024-11-18 15:44:24 +0200

    finishing the project

--- Unsplash Images With React/src/context.jsx
+++ Unsplash Images With React/src/context.jsx
@@ -2,19 +2,31 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 
-const getIntialDarkMode = () => {
-  const preferedDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
-  console.log(preferedDarkMode)
-  return preferedDarkMode
-}
+// const getIntialDarkMode = () => {
+//   const preferedDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+//   console.log(preferedDarkMode)
+  
+//   return preferedDarkMode
+// }
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const storedDarkMode = localStorage.getItem('darkTheme');
+
+  if (storedDarkMode === null) {
+    return prefersDarkMode;
+  }
+
+  return storedDarkMode === 'true';
+};
 
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-  getIntialDarkMode()
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  
   const [searchTerm, setSearchTerm] = useState('cat')
   const toggleTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
   };
   useEffect(() => {
     document.body.classList.toggle("dark-theme", isDarkTheme);
